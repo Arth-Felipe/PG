1d4a404673fd2b5e1bba5f9ed05523d8fe061efe
Arthur <arthur.felipe.00@outlook.com>
2021-10-30 22:27:17 -0300

    finalização do nó de torus

--- js/projeto3.js
+++ js/projeto3.js
@@ -1,23 +1,28 @@
+// Criando a cena e posicionando a câmera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 50;
 
+// Renderizando
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 /*5*/, 6 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-const torusKnot = new THREE.Mesh( geometry, material );
+// Criando o objeto do nó de torus (toro ou toróide)
+const torusGeometry = new THREE.TorusKnotGeometry( 10, 3, 31, 4, 6, 3 );
+const torusMaterial = new THREE.MeshPhongMaterial( { color: 0xff0080 } );
+const torusKnot = new THREE.Mesh( torusGeometry, torusMaterial );
+torusKnot.rotation.set(0.3, 2.9, 0);
 scene.add( torusKnot );
 
-// cube.translateX(-2);
+// Adição de luz à cena
+var light = new THREE.PointLight(0xFFFFFF);
+light.position.set(-10, 15, 50);
+scene.add(light);
 
-camera.position.z = 50;
-
+// Função de animação do objeto
 function animate() {
 	requestAnimationFrame( animate );
-    torusKnot.rotation.x += 0.01;
-    torusKnot.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 animate();
