891fbff169a457fd69be1ab5a87e077d70f65c4c
Crismerlyn <crismerlyn.paiva@gmail.com>
2021-11-01 14:28:08 -0300

    adicionei a pirâmide à cena

--- js/projeto3.js
+++ js/projeto3.js
@@ -15,6 +15,18 @@ const torusKnot = new THREE.Mesh( torusGeometry, torusMaterial );
 torusKnot.rotation.set(0.3, 2.9, 0);
 scene.add( torusKnot );
 
+// Criando o objeto pirâmide
+const piramideGeometry = new THREE.TetrahedronGeometry();
+const piramideMaterial = new THREE.MeshPhongMaterial( { color: 0x993399 } );
+const piramide = new THREE.Mesh( piramideGeometry, piramideMaterial );
+scene.add( piramide );
+// translação do objeto
+piramide.translateX(40);
+// escala do objeto
+piramide.scale.set(10, 10, 10);
+// rotação do objeto
+piramide.rotation.set(0, 1, 1);
+
 // Adição de luz à cena
 var light = new THREE.PointLight(0xFFFFFF);
 light.position.set(-10, 15, 50);
