f46ba7309ed6f0eae37d66364cf6b454c2e7ec53
MiguelBraz <miguelbraz11@hotmail.com>
2021-11-26 01:30:58 -0300

    Base

--- js/projeto3.js
+++ js/projeto3.js
@@ -1,124 +1,28 @@
-/*
-import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
-import {GUI} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/libs/dat.gui.module.js';
-
-let x, y, z
-
-// Criando a cena e posicionando a câmera
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-// Renderizando
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor( 0x3d3c3c );
-document.body.appendChild(renderer.domElement);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-camera.position.set(0, 20, 100);
-controls.update();
-
-// Adição de luz à cena
-var light = new THREE.PointLight(0xFFFFFF);
-light.position.set(-10, 15, 50);
-scene.add(light);
-
-var light2 = new THREE.PointLight(0xFFFFFF);
-light2.position.set(5, 40, -50);
-scene.add(light2);
-
-var ambientLight = new THREE.AmbientLight( 0xffffff, 0.1);
-scene.add(ambientLight);
-
-// Criando o objeto do nó de torus (toro ou toróide)
-const torusGeometry = new THREE.TorusKnotGeometry( 10, 3, 31, 4, 6, 3 );
-const torusMaterial = new THREE.MeshPhongMaterial( { color: 0xff0080 } );
-const torusKnot = new THREE.Mesh( torusGeometry, torusMaterial );
-torusKnot.rotation.set(0.3, 2.9, 0);
-scene.add( torusKnot );
-
-// Criando o objeto pirâmide
-const piramideGeometry = new THREE.TetrahedronGeometry();
-const piramideMaterial = new THREE.MeshPhongMaterial( { color: 0x993399 } );
-const piramide = new THREE.Mesh( piramideGeometry, piramideMaterial );
-scene.add( piramide );
-
-//Criando o objeto círculo
-const esferaGeometry = new THREE.SphereGeometry( 5, 32, 20 );
-const esferaMaterial = new THREE.MeshPhongMaterial( { color: 0x5589f2 } );
-const esfera = new THREE.Mesh( esferaGeometry, esferaMaterial );
-scene.add( esfera );
-
-//Criando retângulo
-const retanguloGeometry = new THREE.BoxGeometry( 1, 2, 1 );
-const retanguloMaterial = new THREE.MeshPhongMaterial( {color: 0x0dfffb} );
-const retangulo = new THREE.Mesh( retanguloGeometry, retanguloMaterial );
-scene.add( retangulo );
-
-// translação do objeto
-piramide.translateX(40);
-esfera.translateX(23);
-
-// escala do objeto
-piramide.scale.set(10, 10, 10);
-esfera.scale.set(1.5, 1.5, 1.5);
-
-// rotação do objeto
-piramide.rotation.set(0, 1, 1);
-esfera.rotation.set(0, 1, 1);
-
-// Criando objeto cilindro
-const geometry = new THREE.CylinderGeometry( 10, 5, 20, 32 );
-const material = new THREE.MeshPhongMaterial( {color: 0xffffff} );
-const cilindro = new THREE.Mesh(geometry, material);
-scene.add(cilindro);
-
-// translação do objeto cilindro
-cilindro.translateX(-25);
-cilindro.translateZ(15);
-// escala do objeto cilindro
-cilindro.scale.set(1, 1.8, 1);
-
-//Translação, rotação e escala do Retângulo
-retangulo.translateX(60)
-retangulo.scale.set(5, 5, 5);
-retangulo.rotation.set(1, 1, 0)
-
-const gui = new GUI()
-const cubeFolder = gui.addFolder('Rotacionar Cilindro')
-cubeFolder.add(cilindro.rotation, 'x', 0, Math.PI * 2)
-cubeFolder.add(cilindro.rotation, 'z', 0, Math.PI * 2)
-cubeFolder.open()
-
-const cameraFolder = gui.addFolder('Zoom da Câmera')
-cameraFolder.add(camera.position, 'z', 20, 100)
-cameraFolder.open()
-*/
-// Função de animação do objeto
-/*
-
-var t = 0;
-function animate() {
-	requestAnimationFrame(animate);
-	spinObject(torusKnot);
-	moveObjectFoward(torusKnot);
-	moveObjectUp(esfera);
-	resizeObject(piramide)
-
-	t += 0.01;  
-	moveObjectOrbital(retangulo, t)
-	renderer.render( scene, camera );
-}
-
-animate();
-*/
-
-
 import * as THREE from 'https://threejs.org/build/three.module.js';
 import Stats from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/libs/stats.module.js';
 
 let stats, scene, renderer, windowWidth, windowHeight
 let mouseX = 0, mouseY = 0;
+
+const geometry = new THREE.CylinderGeometry( 10, 5, 20, 32 );
+const material = new THREE.MeshPhongMaterial( {color: 0xffffff} );
+const cilindro = new THREE.Mesh(geometry, material);
+
+const torusGeometry = new THREE.TorusKnotGeometry( 10, 3, 31, 4, 6, 3 );
+const torusMaterial = new THREE.MeshPhongMaterial( { color: 0xff0080 } );
+const torusKnot = new THREE.Mesh( torusGeometry, torusMaterial );
+
+const piramideGeometry = new THREE.TetrahedronGeometry();
+const piramideMaterial = new THREE.MeshPhongMaterial( { color: 0x993399 } );
+const piramide = new THREE.Mesh( piramideGeometry, piramideMaterial );
+
+const esferaGeometry = new THREE.SphereGeometry( 5, 32, 20 );
+const esferaMaterial = new THREE.MeshPhongMaterial( { color: 0x5589f2 } );
+const esfera = new THREE.Mesh( esferaGeometry, esferaMaterial );
+
+const retanguloGeometry = new THREE.BoxGeometry( 1, 2, 1 );
+const retanguloMaterial = new THREE.MeshPhongMaterial( {color: 0x0dfffb} );
+const retangulo = new THREE.Mesh( retanguloGeometry, retanguloMaterial );
 
 const views = [
 	{
@@ -193,31 +97,23 @@ scene.add(light2);
 var ambientLight = new THREE.AmbientLight( 0xffffff, 0.1);
 scene.add(ambientLight);
 
+async function iniciando(){
 // Criando o objeto do nó de torus (toro ou toróide)
-const torusGeometry = new THREE.TorusKnotGeometry( 10, 3, 31, 4, 6, 3 );
-const torusMaterial = new THREE.MeshPhongMaterial( { color: 0xff0080 } );
-const torusKnot = new THREE.Mesh( torusGeometry, torusMaterial );
+
 torusKnot.rotation.set(0.3, 2.9, 0);
 scene.add( torusKnot );
 
 // Criando o objeto pirâmide
-const piramideGeometry = new THREE.TetrahedronGeometry();
-const piramideMaterial = new THREE.MeshPhongMaterial( { color: 0x993399 } );
-const piramide = new THREE.Mesh( piramideGeometry, piramideMaterial );
+
 scene.add( piramide );
 
 //Criando o objeto círculo
-const esferaGeometry = new THREE.SphereGeometry( 5, 32, 20 );
-const esferaMaterial = new THREE.MeshPhongMaterial( { color: 0x5589f2 } );
-const esfera = new THREE.Mesh( esferaGeometry, esferaMaterial );
+
 scene.add( esfera );
 
 //Criando retângulo
-const retanguloGeometry = new THREE.BoxGeometry( 1, 2, 1 );
-const retanguloMaterial = new THREE.MeshPhongMaterial( {color: 0x0dfffb} );
-const retangulo = new THREE.Mesh( retanguloGeometry, retanguloMaterial );
-scene.add( retangulo );
 
+scene.add( retangulo );
 // translação do objeto
 piramide.translateX(40);
 esfera.translateX(23);
@@ -231,9 +127,7 @@ piramide.rotation.set(0, 1, 1);
 esfera.rotation.set(0, 1, 1);
 
 // Criando objeto cilindro
-const geometry = new THREE.CylinderGeometry( 10, 5, 20, 32 );
-const material = new THREE.MeshPhongMaterial( {color: 0xffffff} );
-const cilindro = new THREE.Mesh(geometry, material);
+
 scene.add(cilindro);
 
 // translação do objeto cilindro
@@ -246,6 +140,9 @@ cilindro.scale.set(1, 1.8, 1);
 retangulo.translateX(60)
 retangulo.scale.set(5, 5, 5);
 retangulo.rotation.set(1, 1, 0)
+}
+
+
 
 renderer = new THREE.WebGLRenderer( {antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -319,14 +216,19 @@ function render() {
 	}
 }
 
-function animate(obj1, obj2, obj3, obj4) {
+async function animate(){
 	render();
 	stats.update();
 	requestAnimationFrame(animate);
-	spinObject(obj1);
-	moveObjectFoward(obj1);
-	moveObjectUp(obj2);
-	resizeObject(obj3)
+	spinObject(torusKnot);
+	moveObjectUp(esfera);
+	moveObjectFoward(torusKnot);
+	resizeObject(piramide);
+	spinObject(retangulo);
 }
 
-animate(torusKnot, esfera, piramide, retangulo);
+
+await iniciando();
+animate()
+// animate(torusKnot, esfera, piramide, retangulo);
+teste();
